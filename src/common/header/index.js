b4abbc62dba85c5fb5e52b985428513d78f30981
1466619197@qq.com <1466619197@qq.com>
2019-06-12 21:37:56 +0800

    change

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,74 +1,128 @@
-import React from 'react';
+import React , {Component} from 'react';
 import { CSSTransition } from 'react-transition-group';
 import {HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button, 
     SearchWrapper, SearchInfo, SearchInfoTitle, SearchInfoHeader, SearchInfoChange,SearchItem, SearchInfoItem} from './style';
 import {connect} from 'react-redux';
 import * as actionCreator from './store/actionCreator';
 
-const getListArea = (show) => {
-    if(show){
+// const getListArea = (show) => {
+//     if(show){
+//         return (
+//             <SearchInfo>
+//                 <SearchInfoHeader>
+//                     <SearchInfoTitle>热门搜索</SearchInfoTitle>
+//                     <SearchInfoChange>换一批</SearchInfoChange>
+//                 </SearchInfoHeader>
+//                 <SearchItem>
+//                         <SearchInfoItem>1ewewe</SearchInfoItem>
+//                         <SearchInfoItem>1weww</SearchInfoItem>
+//                         <SearchInfoItem>1</SearchInfoItem>
+//                         <SearchInfoItem>1111111</SearchInfoItem>
+//                         <SearchInfoItem>111111</SearchInfoItem>
+//                         <SearchInfoItem>11111111</SearchInfoItem>
+//                         <SearchInfoItem>11111</SearchInfoItem>
+//                         <SearchInfoItem>1111</SearchInfoItem>
+//                         <SearchInfoItem>1</SearchInfoItem>
+//                 </SearchItem>
+//             </SearchInfo>
+//         )
+//     }else {
+//         return;
+//     }
+// }
+class Header extends Component{
+    getListArea = (show) => {
+        const {list} = this.props;
+        if(show){
+            return (
+                <SearchInfo>
+                    <SearchInfoHeader>
+                        <SearchInfoTitle>热门搜索</SearchInfoTitle>
+                        <SearchInfoChange>换一批</SearchInfoChange>
+                    </SearchInfoHeader>
+                    <SearchItem>
+                        { list.map( (item) => {
+                           return  <SearchInfoItem key={item}>{item}</SearchInfoItem>
+                        })}
+                    </SearchItem>
+                </SearchInfo>
+            )
+        }else {
+            return;
+        }
+    }
+    render() {
+        const {focusSearch, blurSearch, isFouces} = this.props;
         return (
-            <SearchInfo>
-                <SearchInfoHeader>
-                    <SearchInfoTitle>热门搜索</SearchInfoTitle>
-                    <SearchInfoChange>换一批</SearchInfoChange>
-                </SearchInfoHeader>
-                <SearchItem>
-                        <SearchInfoItem>1ewewe</SearchInfoItem>
-                        <SearchInfoItem>1weww</SearchInfoItem>
-                        <SearchInfoItem>1</SearchInfoItem>
-                        <SearchInfoItem>1111111</SearchInfoItem>
-                        <SearchInfoItem>111111</SearchInfoItem>
-                        <SearchInfoItem>11111111</SearchInfoItem>
-                        <SearchInfoItem>11111</SearchInfoItem>
-                        <SearchInfoItem>1111</SearchInfoItem>
-                        <SearchInfoItem>1</SearchInfoItem>
-                </SearchItem>
-            </SearchInfo>
+            <HeaderWrapper>
+                <Logo/>
+                <Nav>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className='left'>下载APP</NavItem>
+                    <NavItem className='right'>登录</NavItem>
+                    <NavItem className='right'><i className='iconfont'>&#xe636;</i></NavItem>
+                    <SearchWrapper>
+                        <CSSTransition timeout={300} classNames='slide' in={isFouces}>
+                            <NavSearch onFocus={focusSearch} onBlur={blurSearch}
+                                className={isFouces === true ? 'focused' : ''}
+                            ></NavSearch>
+                        </CSSTransition>
+                        <i className={isFouces === true ? 'focused iconfont' : 'iconfont'}>&#xe613;</i>
+                        {this.getListArea(isFouces)}
+                    </SearchWrapper>
+                </Nav>
+                <Addition>
+                    <Button className='reg'>注册</Button>
+                    <Button className='writting'>
+                        <i className='iconfont'>&#xe615;</i>
+                        写文章
+                    </Button>
+                </Addition>
+            </HeaderWrapper>
         )
-    }else {
-        return;
     }
 }
-const Header = (props) => {
-    return (
-        <HeaderWrapper>
-            <Logo/>
-            <Nav>
-                <NavItem className='left active'>首页</NavItem>
-                <NavItem className='left'>下载APP</NavItem>
-                <NavItem className='right'>登录</NavItem>
-                <NavItem className='right'><i className='iconfont'>&#xe636;</i></NavItem>
-                <SearchWrapper>
-                    <CSSTransition timeout={300} classNames='slide' in={props.isFouces}>
-                        <NavSearch onFocus={props.focusSearch} onBlur={props.blurSearch}
-                            className={props.isFouces === true ? 'focused' : ''}
-                        ></NavSearch>
-                    </CSSTransition>
-                    <i className={props.isFouces === true ? 'focused iconfont' : 'iconfont'}>&#xe613;</i>
-                    {getListArea(props.isFouces)}
-                </SearchWrapper>
-            </Nav>
-            <Addition>
-                <Button className='reg'>注册</Button>
-                <Button className='writting'>
-                    <i className='iconfont'>&#xe615;</i>
-                    写文章
-                </Button>
-            </Addition>
-        </HeaderWrapper>
-    )
-}
+// const Header = (props) => {
+//     return (
+//         <HeaderWrapper>
+//             <Logo/>
+//             <Nav>
+//                 <NavItem className='left active'>首页</NavItem>
+//                 <NavItem className='left'>下载APP</NavItem>
+//                 <NavItem className='right'>登录</NavItem>
+//                 <NavItem className='right'><i className='iconfont'>&#xe636;</i></NavItem>
+//                 <SearchWrapper>
+//                     <CSSTransition timeout={300} classNames='slide' in={props.isFouces}>
+//                         <NavSearch onFocus={props.focusSearch} onBlur={props.blurSearch}
+//                             className={props.isFouces === true ? 'focused' : ''}
+//                         ></NavSearch>
+//                     </CSSTransition>
+//                     <i className={props.isFouces === true ? 'focused iconfont' : 'iconfont'}>&#xe613;</i>
+//                     {getListArea(props.isFouces)}
+//                 </SearchWrapper>
+//             </Nav>
+//             <Addition>
+//                 <Button className='reg'>注册</Button>
+//                 <Button className='writting'>
+//                     <i className='iconfont'>&#xe615;</i>
+//                     写文章
+//                 </Button>
+//             </Addition>
+//         </HeaderWrapper>
+//     )
+// }
 
 const mapStateToProps = (state) => {
     return {
-        isFouces: state.header.get("focused")
+        isFouces: state.header.get("focused"),
+        list: state.header.get("list")
     }
 }
 const mapDispathToProps = (dispatch) => {
     return {
         focusSearch(){
             dispatch(actionCreator.searchFocus());
+            dispatch(actionCreator.getList());
         },
         blurSearch(){
             dispatch(actionCreator.searchBlur());
--- src/common/header/store/actionCreator.js
+++ src/common/header/store/actionCreator.js
@@ -1,7 +1,25 @@
-import * as constants from './constants'
+import * as constants from './constants';
+import axios from 'axios';
+import {fromJS} from 'immutable';
 export const searchFocus = () => ({
     type: constants.SEARCH_FOCUS
 });
 export const searchBlur = () => ({
     type: constants.SEARCH_BLUR
 });
+const changeList = (data) => ({
+    type: constants.CHANGE_LIST,
+    data: fromJS(data)
+})
+export const getList = () => {
+    return (dispatch) => {
+        axios.get("/api/headerList.json").then((res) => {
+            const data = res.data;
+            const action = changeList(data.data);
+            dispatch(action);
+        }).catch(() => {
+
+        })
+    }
+    
+}
